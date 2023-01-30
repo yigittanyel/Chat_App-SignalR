fb5bbc586bd23005cb9cd3962544d961987c8867
yigittanyel <tanyelyigit@gmail.com>
2023-01-30 20:07:36 +0300

    initial create

---
+++ SignalRProject.Server/Hubs/MyHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRProject.Hubs
+{
+    public class MyHub : Hub
+    {
+        public static List<string> Messages { get; set; } = new List<string>();
+        public async Task SendMessage(string user,string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage",user,message);
+        }
+
+        public async Task GetMessage()
+        {
+            await Clients.All.SendAsync("ReceiveMessage",Messages);
+        }
+    }
+}
---
+++ SignalRProject.Server/Program.cs
@@ -0,0 +1,49 @@
+using SignalRProject.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+
+#region --
+builder.Services.AddSignalR();
+builder.Services.AddCors();
+#endregion
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(options =>
+     options.WithOrigins("https://localhost:7291")
+            .AllowAnyHeader()
+            .AllowCredentials()
+            .AllowAnyMethod());
+
+app.UseRouting();
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+#region --
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapHub<MyHub>("/MyHub");
+});
+
+app.UseCors("SignalRCorsPolicy");
+#endregion
+
+app.MapControllers();
+
+app.Run();
